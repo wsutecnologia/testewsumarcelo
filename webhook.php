8bc3eac8b4679f0ce29517f08163a1560e677ea3
root <root@redrabbittech.com>
2017-06-23 12:39:49 -0400

    1

---
+++ webhook.php
@@ -0,0 +1,17 @@
+<?php
+
+$event = $_SERVER['HTTP_X_GITHUB_EVENT'];
+$payload = $_POST['payload'];
+$all = "";
+foreach ($_POST as $key => $value) {
+  $all = $key.":".$value.", ";
+}
+
+$data = $event."  ".$payload."  ".$all;
+file_put_contents('/var/www/html/teste/testewsumarcelo/push.log', $data, FILE_APPEND);
+
+if ( $_POST['payload'] ) {
+ # shell_exec( 'cd /srv/www/git-repo/ && git reset --hard HEAD && git pull' );
+}
+
+?>
