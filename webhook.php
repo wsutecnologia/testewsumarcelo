90f5de424dc78684771dadc2a6d133d523842ab4
root <root@redrabbittech.com>
2017-06-23 12:48:16 -0400

    commit

--- webhook.php
+++ webhook.php
@@ -1,18 +1,17 @@
 <?php
 
 $event = $_SERVER['HTTP_X_GITHUB_EVENT'];
-file_put_contents('/var/www/html/teste/testewsumarcelo/push.log', $event, FILE_APPEND);
+file_put_contents('/var/www/html/teste/testewsumarcelo/push.log', $event.PHP_EOL, FILE_APPEND);
 
 #$payload = $_POST['payload'];
-file_put_contents('/var/www/html/teste/testewsumarcelo/push.log', $payload, FILE_APPEND);
+file_put_contents('/var/www/html/teste/testewsumarcelo/push.log', $payload.PHP_EOL, FILE_APPEND);
 
-#$all = "";
-#foreach ($_POST as $key => $value) {
-#  $all = $key.":".$value.", ";
-#}
+$data = "";
+foreach ($_POST as $key => $value) {
+  $data = $key.":".$value.", ";
+}
 
-#$data = $event."  ".$payload."  ".$all;
-#file_put_contents('/var/www/html/teste/testewsumarcelo/push.log', $data, FILE_APPEND);
+file_put_contents('/var/www/html/teste/testewsumarcelo/push.log', $data.PHP_EOL, FILE_APPEND);
 
 #if ( $_POST['payload'] ) {
  # shell_exec( 'cd /srv/www/git-repo/ && git reset --hard HEAD && git pull' );
